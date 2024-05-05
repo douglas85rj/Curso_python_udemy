b202bf3977237979593c8147576632bdce786ed4
Douglas Souza <douglas.souza_tobrasil@prestador.globo>
2024-05-05 20:27:13 -0300

    if_else

---
+++ if_else.py
@@ -0,0 +1,11 @@
+entrada = input ('Vocë quer entrar ou sair?')
+
+if entrada == 'entrar':
+    print('Vocë entrou no sistema!')
+elif entrada == 'sair':
+    print('Vocë saiu do sistema!')
+else:
+    print('Voce não escolheu nenhuma opcão')
+    
+    
+    
---
+++ if_else_2.py
@@ -0,0 +1,17 @@
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+
+if condicao1:
+    print('Código da condicao 1')
+elif condicao2:
+    print('Código da condicao 2')
+elif condicao3:
+    print('Código da condicao 3')
+elif condicao4:
+    print('Código da condicao 4')    
+    
+else:
+    print('Nenhuma condicao verdadeira encontrada')
