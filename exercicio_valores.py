60d2d1901fd7341441540febb3e24b4d985fb5eb
Douglas Souza <douglas.souza_tobrasil@prestador.globo>
2024-05-06 16:08:44 -0300

    comparandoValores

---
+++ exercicio_valores.py
@@ -0,0 +1,11 @@
+primeiro_valor = input('Digite um valor:')
+segundo_valor = input ('Digite o segundo valor:')
+
+
+if primeiro_valor >= segundo_valor:
+    print(f'o primeiro valor {primeiro_valor} é maior ou igual que o segundo valor {segundo_valor}')
+elif segundo_valor >= primeiro_valor:
+    print(f'O segundo valor {segundo_valor} é maior ou igual que o primeiro valor {primeiro_valor}')
+else:
+    print('Bye')
+    
